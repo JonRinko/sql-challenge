337cb4a74bdcf6214c26f8cbda7a4103782f07b0
Jonathan_Rinko <jonjr23@gmail.com>
2019-10-13 02:45:38 -0400

    added schemas/changed keys

--- db/mysqldb.sql
+++ db/mysqldb.sql
@@ -1,25 +1,35 @@
--- drop table departments;
--- drop table dept_emp;
--- drop table employees;
--- drop table dept_manager;
--- drop table titles;
--- drop table salaries;
+-- keep some drops handy as I build and rebuild
+-- drop table departments CASCADE;
+-- drop table dept_emp CASCADE;
+-- drop table employees CASCADE;
+-- drop table dept_manager CASCADE;
+-- drop table titles CASCADE;
+-- drop table salaries CASCADE;
+CREATE SCHEMA DepartmentData;
+CREATE SCHEMA EmployeeData;
 
-
-CREATE TABLE departments as d(
-dept_no varchar(100) PK NOT NULL,
-dept_name varchar(100) NOT NULL
+CREATE TABLE DepartmentData.departments (
+dept_no varchar(100) primary key,
+dept_name varchar(100)
 );
 
-CREATE TABLE dept_emp(
-emp_no int PK NOT NULL,
-dept_no varchar(100) REFERENCES d[dept_no],
-from_date date NOT NULL,
+CREATE TABLE DepartmentData.dept_emp(
+emp_no varchar primary key,
+dept_no varchar(100) REFERENCES DepartmentData.departments(dept_no),
+from_date date,
 to_date date
 );
 
-CREATE TABLE employees(
+CREATE TABLE DepartmentData.dept_manager (
+dept_no varchar(100) REFERENCES DepartmentData.departments(dept_no),
 emp_no int,
+from_date date,
+to_date date
+);
+
+
+CREATE TABLE EmployeeData.employees (
+emp_no int primary key,
 first_name varchar(100),
 last_name varchar(100),
 birth_date date,
@@ -27,27 +37,22 @@ gender varchar(100),
 hire_date date
 );
 
-CREATE TABLE dept_manager(
-dept_no varchar(100),
-emp_no int,
-from_date date NOT NULL,
-to_date date
-);
-
-CREATE TABLE titles(
-emp_no int,
+CREATE TABLE EmployeeData.titles(
+emp_no int REFERENCES EmployeeData.employees(emp_no),
 title varchar(100),
-from_date date NOT NULL,
+from_date date,
 to_date date
 );
 
-CREATE TABLE salaries(
-emp_no int,
-salary decimal(18,2),
-from_date date NOT NULL,
+CREATE TABLE EmployeeData.salaries(
+emp_no int REFERENCES EmployeeData.employees(emp_no),
+salary decimal(16,2),
+from_date date,
 to_date date
 );
 
 -- use below code to alter/add fk
 -- ALTER TABLE ORDERS 
 --    ADD FOREIGN KEY (Customer_ID) REFERENCES CUSTOMERS (ID);
+
+-- now we can add the csv files to our existing tables
